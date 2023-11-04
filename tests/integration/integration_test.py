96ee677d68e1f7d7c7bf54c3819c7731fcbf900f
geogreen-dev <green.george.95@gmail.com>
2023-11-04 11:53:41 +0000

    fixed integration test dependency

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -5,22 +5,15 @@ from src.match import Match
 from src import val_cal
 
 class IntegrationTest(unittest.TestCase):
-	
-	MATCH_PAGE=""
 
-	def test_get_match_links(self):
+	def test_core_functionality(self):
 		matches = val_cal.get_match_links_by_id("https://www.vlr.gg/matches?page={}")
 		# we at least get some matches back and they resolve to valid URLs
 		self.assertTrue(len(matches)>1)
 		for k, v in matches.items():
 			self.assertTrue(validators.url(v))
-			MATCH_PAGE=v
-
-	def test_parse_match_all_known(self):
-		matches = val_cal.get_match_links_by_id("https://www.vlr.gg/matches?page={}")
-
-		# running against all matches can be quite slow
-		for match in list(matches.values())[:10]:
+		
+		for match in (list(matches.values())[:10]):
 			if match == None: pass
 			else: val_cal.parse_match_page(match)
 
